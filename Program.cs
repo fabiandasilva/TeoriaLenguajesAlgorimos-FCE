7e1a1cbb64b6dba7c87816ed75d33f4cb1467abd
Fabian Da Silva <fabian.da.silva@hotmail.com>
2023-02-03 09:25:58 -0300

    Clase 2

---
+++ Program.cs
@@ -0,0 +1,68 @@
+﻿internal class Program
+{
+    private static string i;
+
+    private static void Main(string[] args)
+    {
+         string dia = "jueves";
+         string hoy = "viernes";
+
+
+
+        //if (dia == hoy)
+        //{
+        //    Console.WriteLine("Bienvenido");
+        //}
+        //else
+        //{
+        //    Console.WriteLine("Chau");
+        //}
+
+        // switch (dia)
+        // {
+        //     case "lunes": Console.WriteLine("Es lunes"); break;
+        //     case "martes": Console.WriteLine("Es martes"); break;
+        //     case "miercoles": Console.WriteLine("Es miercoles"); break;
+        //     case "jueves": Console.WriteLine("Es jueves"); break;
+        //     case "viernes": Console.WriteLine("Es viernes"); break;
+        //     case "sabado": Console.WriteLine("Es sabado"); break;
+        //     case "domingo": Console.WriteLine("Es domingo"); break;
+        //     default: Console.WriteLine("No es un dia de la semana"); break;
+        // }
+
+
+
+      //  int ladoX = int.Parse(Console.ReadLine( ));
+      //  int ladoY = int.Parse(Console.ReadLine( ));
+//
+      //  if (ladoX == ladoY)
+      //  {
+      //      Console.WriteLine("Es un cuadrado");
+      //  }
+      //  else
+      //  {
+      //      Console.WriteLine("Es un rectangulo");
+      //      Console.ReadKey();
+      //  }
+
+        for (int i = 0; i < 5 ; i++);
+        {
+            //Recorrer todos los dias
+            Console.WriteLine("Dia: " + i);
+            
+
+        }
+
+
+
+
+    }
+}
+
+
+//Estructura de decision
+
+//If (condicion){}
+//if(condicion){}else{condicion 2}
+//if(condicion){}else if(condicion){}else{condicion}
+
